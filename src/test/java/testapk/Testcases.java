6aa5e6bf6feb9ff00cf25b137b260fb7e1c11deb
Muhammad Haris <engrmharis91@gmail.com>
2019-12-09 16:51:34 +0500

    Alignment

--- src/test/java/testapk/Testcases.java
+++ src/test/java/testapk/Testcases.java
@@ -23,27 +23,26 @@ import io.qameta.allure.SeverityLevel;
 import pageObjects.appobjects;
 
 public class Testcases {
-	
-	
+
+
 	@BeforeSuite
 	@Severity(SeverityLevel.BLOCKER) 
 	//-----------------------------------------------Starts appium and emulator-------------------------------------------------------//
 	public void startappemu() throws InterruptedException, IOException {
-	ProcessBuilder pb1 = new ProcessBuilder("cmd", "/c", "appiumemulator.bat");
-	File dir = new File("src");
-	pb1.directory(dir);
-	Process p1 = pb1.start();
-	Thread.sleep(10000);
-	System.out.println("Appium and Emulator started succcesfully");
+		ProcessBuilder pb1 = new ProcessBuilder("cmd", "/c", "appiumemulator.bat");
+		File dir = new File("src");
+		pb1.directory(dir);
+		Process p1 = pb1.start();
+		Thread.sleep(10000);
+		System.out.println("Appium and Emulator started succcesfully");
 	}
-	
-	
+
+
 	@BeforeTest
 	@Severity(SeverityLevel.BLOCKER) 
 	public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException
 	//-----------------------------------------------Driver function-------------------------------------------------------//
 	{
-		
 		File file = new File("src");
 		File location = new File(file,"app-release-signed.apk");
 		DesiredCapabilities cap = new DesiredCapabilities();
@@ -53,15 +52,13 @@ public class Testcases {
 		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
 		System.out.println("driver testcase");
 		return driver;
-		
 	}
-	
-	
+
+
 	@Test(priority=1)
 	@Severity(SeverityLevel.CRITICAL)
 	//-----------------------------------------------Correct email and correct password-------------------------------------------------------//
 	public void tc1() throws MalformedURLException, InterruptedException {     
-
 		AndroidDriver<AndroidElement> driver=Capabilities();
 		appobjects ao=new appobjects(driver);
 		WebDriverWait loadwait = new WebDriverWait(driver, 30);
@@ -76,11 +73,12 @@ public class Testcases {
 
 
 	}
+
+
 	@Test(priority=2)
 	@Severity(SeverityLevel.CRITICAL)
 	//-----------------------------------------------Incorrect email-------------------------------------------------------//
 	public void tc2() throws MalformedURLException, InterruptedException {     
-
 		AndroidDriver<AndroidElement> driver=Capabilities();
 		appobjects ao=new appobjects(driver);
 		WebDriverWait loadwait = new WebDriverWait(driver, 30);
@@ -92,15 +90,13 @@ public class Testcases {
 		String toasttext = ao.checkloginstatus.getText();
 		System.out.println(toasttext);
 		Assert.assertEquals("Email not valid", toasttext);
-
-
 	}
+
 
 	@Test(priority=3)
 	@Severity(SeverityLevel.NORMAL)
 	//-----------------------------------------------empty email and empty password-------------------------------------------------------//
 	public void tc3() throws MalformedURLException, InterruptedException {     
-
 		AndroidDriver<AndroidElement> driver=Capabilities();
 		appobjects ao=new appobjects(driver);
 		WebDriverWait loadwait = new WebDriverWait(driver, 30);
@@ -110,14 +106,13 @@ public class Testcases {
 		String toasttext = ao.checkloginstatus.getText();
 		System.out.println(toasttext);
 		Assert.assertEquals("Please enter valid email and password", toasttext);
-
-
 	}
+
+
 	@Test(priority=4)
 	@Severity(SeverityLevel.MINOR)
 	//-----------------------------------------------correct email and empty password-------------------------------------------------------//
 	public void tc4() throws MalformedURLException, InterruptedException {     
-
 		AndroidDriver<AndroidElement> driver=Capabilities();
 		appobjects ao=new appobjects(driver);
 		WebDriverWait loadwait = new WebDriverWait(driver, 30);
@@ -128,27 +123,26 @@ public class Testcases {
 		String toasttext = ao.checkloginstatus.getText();
 		System.out.println(toasttext);
 		Assert.assertEquals("Please enter valid email and password", toasttext);
-
-
 	}
+
+
 	@Test(priority=5)
 	@Severity(SeverityLevel.TRIVIAL)
 	//-----------------------------------------------correct email and empty password-------------------------------------------------------//
 	public void tc5() throws MalformedURLException, InterruptedException {     
-
 		AndroidDriver<AndroidElement> driver=Capabilities();
 		System.out.println("Test jenkinsCIapp new");
-		
-
 	}
+
+
 	@AfterSuite
 	@Severity(SeverityLevel.BLOCKER) 
 	//-----------------------------------------------Stops appium and emulator-------------------------------------------------------//
 	public void stopappemu() throws InterruptedException, IOException {
-	ProcessBuilder pb2 = new ProcessBuilder("cmd", "/c", "stopappiumemulator.bat");
-	File dir2 = new File("src");
-	pb2.directory(dir2);
-	Process p2 = pb2.start();
-	System.out.println("Appium and Emulator stopped succcesfully");
+		ProcessBuilder pb2 = new ProcessBuilder("cmd", "/c", "stopappiumemulator.bat");
+		File dir2 = new File("src");
+		pb2.directory(dir2);
+		Process p2 = pb2.start();
+		System.out.println("Appium and Emulator stopped succcesfully");
 	}
 }
